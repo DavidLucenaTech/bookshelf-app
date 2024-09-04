48092e50e606b858c71726b7a9f6d2af75113dfe
David <davidlr.tech@gmail.com>
2024-09-04 18:55:32 +0200

    Add empyrean saga

--- src/components/empyrean/Empyrean.jsx
+++ src/components/empyrean/Empyrean.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import './Empyrean.css'
+import { BookSheet } from '../booksheet/BookSheet'
 
 export function Empyrean() {
+    const [books, setBooks] = useState([])
+
+    const getEmpyreanBooks = () => {
+        fetch('./RY_empyrean.json'
+            ,{
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(myJson) {
+            console.log(myJson)
+            setBooks(myJson)
+        })
+    }
+
+    useEffect(() => {
+        getEmpyreanBooks()
+    }, [])
+
     return (
         <div>
-            This is the Empyrean component
+            <h1>Empyrean</h1>
+            <div className='grid'>
+                {
+                    books && books.length > 0 && books.map((book)=><BookSheet {...book} key={book.id}/>)
+                }
+            </div>
         </div>
     )
 }
+
