13b446d9d6bec6c6546eb8ccdf8167317b3d3730
David <davidlr.tech@gmail.com>
2024-09-04 18:55:23 +0200

    Add the name of wind saga

--- src/components/thenameofwind/Thenameofwind.jsx
+++ src/components/thenameofwind/Thenameofwind.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import './Thenameofwind.css'
+import { BookSheet } from '../booksheet/BookSheet'
+
 
 export function Thenameofwind() {
+    const [books, setBooks] = useState([])
+
+    const getTheNameOfWindBooks = () => {
+        fetch('./PR_name_of_wind.json'
+            ,{
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(myJson) {
+            console.log(myJson)
+            setBooks(myJson)
+        })
+    }
+
+    useEffect(() => {
+        getTheNameOfWindBooks()
+    }, [])
+
     return (
         <div>
-            This is the The name of wind component
+            <h1>The Name of Wind</h1>
+            <div className='grid'>
+                {
+                    books && books.length > 0 && books.map((book)=><BookSheet {...book} key={book.id}/>)
+                }
+            </div>
         </div>
     )
 }
