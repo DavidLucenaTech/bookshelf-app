132d7b9c41e76c44f76d8990b838a332ce466fd3
David <davidlr.tech@gmail.com>
2024-09-07 17:09:18 +0200

    Add throne of glass

--- src/components/throneofglass/Throneofglass.jsx
+++ src/components/throneofglass/Throneofglass.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import './Throneofglass.css'
+import { BookSheet } from '../booksheet/BookSheet'
 
 export function Throneofglass() {
+    const [books, setBooks] = useState([])
+
+    const getEmpyreanBooks = () => {
+        fetch('./SM_throne_of_glass.json'
+            ,{
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(myJson) {
+            console.log(myJson)
+            setBooks(myJson)
+        })
+    }
+
+    useEffect(() => {
+        getEmpyreanBooks()
+    }, [])
+
     return (
         <div>
-            This is the Throne of glass component
+            <h1>Throne of Glass</h1>
+            <div className='grid'>
+                {
+                    books && books.length > 0 && books.map((book)=><BookSheet {...book} key={book.id}/>)
+                }
+            </div>
         </div>
     )
 }
